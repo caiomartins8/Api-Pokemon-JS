ba6f3e4230230c48aa104b93f1392b93125f24d7
Caio Martins <caiom7436@gmail.com>
2025-11-27 18:09:20 -0300

    projeto api

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 let nomepokemon = document.getElementById('poke-nome');
-let imgpokemon = document.getElementById('poke-img');
+let imgpokemon = document.getElementById('poke-img'); // corrigi o ID aqui que vc tinha arrumado no HTML
 //variavel puxando nome e img do html
 let inputpokemon = document.getElementById('poke-input');
 let btnpokemon = document.getElementById('poke-btn');
 
-btnpokemon.addEventListener('click' , buscarPokemon);
+btnpokemon.addEventListener('click', buscarPokemon);
 //adiciona evento de click no botao para chamar a funcao buscarPokemon
 
 async function buscarPokemon() {
     //funcao assincrona para buscar o pokemon na api
     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputpokemon.value.toLowerCase()}`);
     //fetch busca o pokemon na api usando o valor do input , tolowercase converte o valor pra minusculo
-    const dados = await resposta.json();
-    //converte a resposta para json
+    
 
-    nomepokemon.innerHTML = dados.name;
-    //coloca o nome do pokemon no html
-    imgpokemon.src = dados.sprites.front_default;
-    //coloca o nome e a imagem do pokemon no html
-    // sprites e front_default sao propriedades da 
-    // api para pegar a imagem do pokemon
 
     if (resposta.ok) {
         const dados = await resposta.json();
         //converte a resposta para json
+        
         nomepokemon.innerHTML = dados.name;
+        //coloca o nome do pokemon no html
         imgpokemon.src = dados.sprites.front_default;
-
+        // o sprites serve para acessar as imagens do pokemon e
+        //  o front_default e a imagem padrao do pokemon
+        
         //se a resposta for ok , coloca o nome e a imagem do pokemon no html
     } else {
         nomepokemon.innerHTML = "Pokemon nao encontrado";
@@ -34,5 +31,3 @@ async function buscarPokemon() {
         //se a resposta nao for ok , mostra a mensagem de erro e uma imagem padrao
     }
 }
-
-buscarPokemon();
